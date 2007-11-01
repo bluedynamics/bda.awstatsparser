bfc733c8a9b442d9eb5191a3f81945eec349b42d
rnix <rnix@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-11-01 09:28:25 +0000

    eggified

--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
-from distutils.core import setup
-setup(name='bda-awstatsparser',
-      packages=['bda', 'bda.awstatsparser'],
-      package_dir={
-        'bda': 'bda_parent',
-        'bda.awstatsparser': '.'
-      }
-)
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0 (svn unreleased)'
+
+setup(name='bda.awstatsparser',
+      version=version,
+      description="library for parsing awstats",
+      long_description="""\
+TODO""",
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Web Environment',
+            'Framework :: Zope2',
+            'License :: OSI Approved :: GNU General Public License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',            
+      ], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Jens Klein',
+      author_email='dev@bluedynamics.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup',]),
+      namespace_packages=['bda',],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',                        
+          # -*- Extra requirements: -*
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
