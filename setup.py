7e74f5bf606d379057bca4156865df0aa6f4ba74
jensens <jensens@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-11-05 10:36:10 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.1 (svn unreleased)'
+version = '1.1'
 
 setup(name='bda.awstatsparser',
       version=version,
-      description="library for parsing awstats",
-      long_description="""\
-TODO""",
+      description="library for parsing of awstats result files",
+      long_description="""""",
       classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Environment :: Web Environment',
-            'Framework :: Zope2',
-            'License :: OSI Approved :: GNU General Public License',
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',            
       ], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Jens Klein',
       author_email='dev@bluedynamics.com',
-      url='',
+      url='http://svn.plone.org/svn/collective/bda.awstatsparser/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup',]),
       namespace_packages=['bda',],
