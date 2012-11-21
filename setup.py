41d421e673ea2fe8cd3e8b627479f983333bf7f5
Robert Niederreiter <office@squarewave.at>
2012-11-21 20:02:01 +0100

    back to dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 )
 
 
-version = '1.2.1htug1'
+version = '1.2.1dev'
 shortdesc = 'Library for parsing of awstats result files'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
